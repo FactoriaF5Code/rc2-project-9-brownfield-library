0d614efc4b7d3f51583c5c0aa5307d105380b5bb
Javi <javi@jsrois.net>
2024-02-20 19:42:34 +0100

    [BL-5] Update git hook

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ cat << 'EOF' > $COMMIT_MSG_HOOK
 #!/bin/sh
 
 # Regular expression to check for feature name in brackets at the start of the commit message
-PATTERN="^\[.+\].*$"
+PATTERN="^\[BL-[0-9]+\].*$"
 
 # Read the commit message from the file provided as the first argument
 COMMIT_MSG_FILE=$1
@@ -17,7 +17,7 @@ COMMIT_MSG=$(cat $COMMIT_MSG_FILE)
 
 # Check if the commit message matches the pattern
 if ! echo "$COMMIT_MSG" | grep -Eq "$PATTERN"; then
-    echo "ERROR: Commit message must start with a feature name in brackets (e.g., '[feature-name] Your message')."
+    echo "ERROR: Commit message must start with a User Story or Task code in brackets (e.g., '[BL-129] Your message')."
     exit 1
 fi
 
