5aadc8f29004c7d44185e00d8bb61d672a0f7eba
Javi <javi@jsrois.net>
2024-02-25 09:40:01 +0100

    [fix] Update spring version and setup script

---
+++ scripts/pre-commit-hook.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Define the path for the commit-msg hook
+HOOKS_DIR=".git/hooks"
+COMMIT_MSG_HOOK="$HOOKS_DIR/commit-msg"
+
+# Create the commit-msg hook with the required check for the feature name in brackets
+cat << 'EOF' > $COMMIT_MSG_HOOK
+#!/bin/sh
+
+# Regular expression to check for feature name in brackets at the start of the commit message
+PATTERN="^\[(BL-[0-9]+|fix)\].*$"
+
+# Read the commit message from the file provided as the first argument
+COMMIT_MSG_FILE=$1
+COMMIT_MSG=$(cat $COMMIT_MSG_FILE)
+
+# Check if the commit message matches the pattern
+if ! echo "$COMMIT_MSG" | grep -Eq "$PATTERN"; then
+    echo "ERROR: Commit message must start with a User Story or Task code in brackets (e.g., '[BL-129] Your message')."
+    exit 1
+fi
+
+exit 0
+EOF
+
+# Make the commit-msg hook executable
+chmod +x $COMMIT_MSG_HOOK
--- setup.sh
+++ setup.sh
@@ -1,30 +1,5 @@
 #!/bin/bash
 
-# Define the path for the commit-msg hook
-HOOKS_DIR=".git/hooks"
-COMMIT_MSG_HOOK="$HOOKS_DIR/commit-msg"
-
-# Create the commit-msg hook with the required check for the feature name in brackets
-cat << 'EOF' > $COMMIT_MSG_HOOK
-#!/bin/sh
-
-# Regular expression to check for feature name in brackets at the start of the commit message
-PATTERN="^\[(BL-[0-9]+|fix)\].*$"
-
-# Read the commit message from the file provided as the first argument
-COMMIT_MSG_FILE=$1
-COMMIT_MSG=$(cat $COMMIT_MSG_FILE)
-
-# Check if the commit message matches the pattern
-if ! echo "$COMMIT_MSG" | grep -Eq "$PATTERN"; then
-    echo "ERROR: Commit message must start with a User Story or Task code in brackets (e.g., '[BL-129] Your message')."
-    exit 1
-fi
-
-exit 0
-EOF
-
-# Make the commit-msg hook executable
-chmod +x $COMMIT_MSG_HOOK
-
-echo "Git hook installed successfully."
+echo -n "Installing pre-commit hook... "
+. ./scripts/pre-commit-hook.sh
+echo "✅"
