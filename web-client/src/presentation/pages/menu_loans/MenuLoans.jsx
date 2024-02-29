a6bacbfd4c27259836628ddd87f840f8a8dab13d
mfg1985 <mfg1985@me.com>
2024-02-29 14:03:33 +0100

    [BL-29]Feat: css MenuLoans

--- web-client/src/presentation/pages/menu_loans/MenuLoans.jsx
+++ web-client/src/presentation/pages/menu_loans/MenuLoans.jsx
@@ -3,15 +3,11 @@ import "./MenuLoans.css";
 import { NewLoanButton } from "../../components/NewLoanButton/NewLoanButton";
 import { GoLoansReturnsButton } from "../../components/GoLoansReturnsButton/GoLoansReturnsButton";
 
-
 export const MenuLoans = () => {
   return (
-    <div>
-        
-        <NewLoanButton />
-        <GoLoansReturnsButton />
-        
+    <div className="button-container">
+      <NewLoanButton />
+      <GoLoansReturnsButton />
     </div>
-  )
-}
-
+  );
+};
