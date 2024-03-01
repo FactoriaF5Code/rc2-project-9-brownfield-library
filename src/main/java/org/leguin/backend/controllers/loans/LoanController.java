c321e6a58fc59cef0628174a3505f4fa560bbdbf
evavelasco <evapibiyal@gmail.com>
2024-03-01 12:32:36 +0100

    [BL-28] añadir metodo en loan repostitory

--- src/main/java/org/leguin/backend/controllers/loans/LoanController.java
+++ src/main/java/org/leguin/backend/controllers/loans/LoanController.java
@@ -62,3 +62,4 @@ public class LoanController {
         } else {
             return ResponseEntity.notFound().build();
         }
+    }
--- src/main/java/org/leguin/backend/persistence/loans/LoanRepository.java
+++ src/main/java/org/leguin/backend/persistence/loans/LoanRepository.java
@@ -1,9 +1,13 @@
 package org.leguin.backend.persistence.loans;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface LoanRepository extends JpaRepository<Loan, UUID>{}
+public interface LoanRepository extends JpaRepository<Loan, UUID> {
+    List<Loan> findByBookId(UUID bookId);
+
+}
