2296f25d05fa2d16fd487f9628c11fc9e0500add
Johnny <jonathanbc2012@gmail.com>
2024-03-05 14:56:34 +0100

    [BL-38] environment variables created, but don't work

--- web-client/vite.config.js
+++ web-client/vite.config.js
@@ -1,5 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { config } from 'dotenv';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { join } from 'path';
+
+try {
+  config({ path: join(__dirname, '.env.local') });
+} catch (error) {
+  console.error('Error loading environment variables:', error);
+}
+
+const jsonServerPort = process.env.JSON_SERVER_PORT || 9001;
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,5 +20,15 @@ export default defineConfig({
     globals: true,
     setupFiles: './tests/setup.js',
     watch: false
-  }
+  },
+  server: {
+        port: 5173,
+    proxy: {      
+      '/api': {
+        target: `http://localhost:${jsonServerPort}`,
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 })
