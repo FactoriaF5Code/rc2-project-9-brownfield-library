0c5e4b81e8f1402c8e2adffdf15c520d46854a52
osaga-fu <151243613+osaga-fu@users.noreply.github.com>
2024-03-07 14:14:31 +0100

    [BL-31] Added csrf disable to Security configuration

--- src/main/java/org/leguin/backend/configuration/SecurityConfiguration.java
+++ src/main/java/org/leguin/backend/configuration/SecurityConfiguration.java
@@ -4,26 +4,22 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.server.SecurityWebFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
-
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
     @Bean
-	public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) throws Exception {
-		http
-        .authorizeHttpRequests((authorize) -> authorize
-        .anyRequest().permitAll()
-        .requestMatchers("/api/**").authenticated()
-        .csrf(csrf -> csrf.disable())
-        )
-        // .httpBasic(withDefaults())
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-		return http.build();
-	}
+        http.csrf((csrf) -> csrf.disable())
+                .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers("/api/**").authenticated()
+                        .anyRequest().permitAll())
+                .httpBasic(withDefaults());
+
+        return http.build();
+    }
 }
