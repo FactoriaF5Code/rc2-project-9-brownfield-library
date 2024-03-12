b02fd0c15cb7187c9b066ec417037d49b021c667
San-QP <s.rebollar.g@gmail.com>
2024-03-12 10:43:51 +0100

    [BL-32] comments deleted

--- src/test/java/org/leguin/backend/integration/BookSearchTest.java
+++ src/test/java/org/leguin/backend/integration/BookSearchTest.java
@@ -57,7 +57,6 @@ class BookSearchTest {
 						jsonPath("$.results[0].author", equalTo("Ursula K. Leguin")),
 						jsonPath("$.results[0].isbn", equalTo("129834751375")),
 						jsonPath("$.results[0].year", equalTo("1969")));
-						/*jsonPath("$.results[0].available", equalTo(true)));*/
 	}
 
 }
